214c27e260448a7c5a77fc2378d7beb3a1ddfc29
MechyKola <io@mykola.cc>
2022-04-28 13:33:45 +0100

    added some appliance csvs

--- test_script.py
+++ test_script.py
@@ -11,10 +11,15 @@ global_appliances = {
     "stove": ContinuousAppliance("stove", 1000, [1, 1.5, 2, 2.5, 3, 3])
 }
 
+with open('appliances_data.csv') as f:
+    output = [float(s) for line in f.readlines() for s in line[:-1].split(',')]
+
 # plotting
 plt.title("Load graph")
 plt.xlabel("t (minute)")
 plt.ylabel("power usage (watts)")
+
+allHouses = [ 0 for _ in range(1440) ]
 
 for _ in range(20):
     test_model = HouseModel([21], global_appliances.values())
@@ -25,5 +30,13 @@ for _ in range(20):
     x = np.arange(0, 1440)
     y = np.array(household_result)
 
+    for j in range(1440):
+        allHouses[j] += household_result[j]
+
     plt.plot(x, y)
+
+x = np.arange(0, 1440)
+y = np.array(allHouses)
+plt.plot(x, y)
+
 plt.show()
